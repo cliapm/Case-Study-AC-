8440c3e699288646dbfbf9ea2f3bbf69ac4fb35e
cliapm <clodagh2@outlook.com>
2026-09-14 16:09:51 +0100

    Create pt.ts

---
+++ src/lib/i18n/pt.ts
@@ -0,0 +1,11 @@
+export const pt = {
+  nav: {
+    participant: "Participante",
+    facilitator: "Facilitador",
+    qr: "QR",
+  },
+  brand: {
+    tag: "Projeto Agua Clara",
+    subtitle: "Simulação",
+  },
+};
