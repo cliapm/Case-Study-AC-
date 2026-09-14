ed5e6a89df75191d116b0029f76de251674628b1
cliapm <clodagh2@outlook.com>
2026-09-14 16:08:06 +0100

    Create en.ts

---
+++ src/lib/i18n/en.ts
@@ -0,0 +1,11 @@
+export const en = {
+  nav: {
+    participant: "Participant",
+    facilitator: "Facilitator",
+    qr: "QR",
+  },
+  brand: {
+    tag: "Project Agua Clara",
+    subtitle: "Simulation",
+  },
+};
