09ef9f1cbf88223fe1610c1ba4b7e3ed20cc52dc
cliapm <clodagh2@outlook.com>
2026-09-14 15:09:00 +0100

    Delete src/lib/src/app/api/stage/route.ts

--- src/lib/src/app/api/stage/route.ts
+++
@@ -1,8 +0,0 @@
-import { getReleasedStage } from "@/lib/kv";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  const stageNumber = await getReleasedStage();
-  return Response.json({ stageNumber });
-}
